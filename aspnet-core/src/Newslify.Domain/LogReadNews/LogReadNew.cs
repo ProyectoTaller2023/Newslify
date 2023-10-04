54b45c2abe63b59d9cc836f661dc32d8aefdf71f
giansito2 <gmb.gian@gmail.com>
2023-10-04 19:02:17 -0300

    Agregar entidades.

---
+++ aspnet-core/src/Newslify.Domain/LogReadNews/LogReadNew.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Identity;
+
+namespace Newslify.LogReadNews
+{
+    public class LogReadNew : Entity<int>
+    {
+        public string Url { get; set; }
+        public DateTime dateRead { get; set; }
+        public Guid userId { get; set; }
+    }
+}
---
+++ aspnet-core/src/Newslify.Domain/News/New.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace Newslify.News
+{
+    public class New : Entity<int>
+    {
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Source { get; set; }
+    }
+}

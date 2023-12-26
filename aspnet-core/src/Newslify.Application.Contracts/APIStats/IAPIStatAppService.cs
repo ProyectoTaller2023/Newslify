0d31c38b40a1ac18dcd95a36c8aeaa1b1cc839dd
Emiliano Lanaro <emilanaro082014@gmail.com>
2023-12-26 18:46:49 -0300

    feat:agregar APIStats

---
+++ aspnet-core/src/Newslify.Application.Contracts/APIStats/IAPIStatAppService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+namespace Newslify.APILogs;
+
+public interface IAPIStatAppService : IApplicationService
+{
+    Task<int> GetLogsAmount();
+    Task<double> GetAverageAPIAccess();
+    Task<(string, int)> GetTrendingTopic();
+}

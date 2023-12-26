79c9205dc14824367b43ac8f054da76c09aa6e7f
Emiliano Lanaro <emilanaro082014@gmail.com>
2023-12-26 17:15:58 -0300

    feat:agregar LogsManager

--- aspnet-core/src/Newslify.Application/News/NewsAppService.cs
+++ aspnet-core/src/Newslify.Application/News/NewsAppService.cs
@@ -1,29 +1,46 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Newslify.APILogs;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Volo.Abp.Application.Services;
+
 
 namespace Newslify
 {
+    [Authorize]
     public class NewsAppService: NewslifyAppService, INewsAppService
     {
 
         private readonly INewsAPI _newsService;
+        private readonly APILogManager _APILogsManager;
+        private readonly UserManager<Volo.Abp.Identity.IdentityUser> _userManager;
 
-        public NewsAppService(INewsAPI newsService)
+        public NewsAppService(INewsAPI newsService, APILogManager APILogsManager, UserManager<Volo.Abp.Identity.IdentityUser> userManager)
         {
             _newsService = newsService;
+            _APILogsManager = APILogsManager;
+            _userManager = userManager;
         }
 
         public async Task<ICollection<NewsDto>> GetNewsAsync(string LanguageIntCode, int? amountNews, string? query, DateTime? dateFrom)
-         {
+        {
             // Aca estaria el monitoreo de tiempo de respuesta de la API para independizar
             //TODO: Control de acceso tiempo a la API
-           ICollection <ArticleDto> articles = await _newsService.getNews(LanguageIntCode.ToUpper(),amountNews, query, dateFrom);// metodo que se conecte a la API y traiga las noticias
-
-           return ObjectMapper.Map<ICollection<ArticleDto>, ICollection<NewsDto>>(articles);
+            var userGuid = CurrentUser.Id.GetValueOrDefault();
+            var User = await _userManager.FindByIdAsync(userGuid.ToString());
+            DateTime startRequest = DateTime.Now;
+            try
+            {
+                ICollection<ArticleDto> articles = await _newsService.getNews(LanguageIntCode.ToUpper(), amountNews, query, dateFrom);// metodo que se conecte a la API y traiga las noticias
+                DateTime endRequest = DateTime.Now;
+                await _APILogsManager.Create(query, startRequest, endRequest, User);
+                return ObjectMapper.Map<ICollection<ArticleDto>, ICollection<NewsDto>>(articles);
+            }
+            catch (Exception ex) 
+            { 
+                return new List<NewsDto>();
+            }
          }
     }
 }
